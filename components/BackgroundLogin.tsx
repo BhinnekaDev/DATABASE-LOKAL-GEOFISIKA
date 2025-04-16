774e899fbfd0c9fe2b948a62f676733b63de6947
fifovalle <fifanaufal10@gmail.com>
2025-04-16 11:12:22 +0700

    chore: tambah deteksi tema terang dan gelap untuk background login

--- components/BackgroundLogin.tsx
+++ components/BackgroundLogin.tsx
@@ -1,19 +1,24 @@
+import { ReactNode } from "react";
+import { useMedia } from "react-use";
 import backgroundLight from "@/public/Background-Light.jpg";
 import backgroundDark from "@/public/Background-Dark.jpg";
 
-export default function BackgroundLogin({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+// Komponen BackgroundLogin menerima children sebagai prop
+export default function BackgroundLogin({ children }: { children: ReactNode }) {
+    // Menggunakan hook useMedia untuk mendeteksi apakah preferensi mode gelap (dark mode) diaktifkan
+    const isDarkMode = useMedia("(prefers-color-scheme: dark)");
+
     return (
         <div
             className="w-screen h-screen bg-cover bg-center flex justify-center items-center"
             style={{
-                backgroundImage: `url(${backgroundDark.src})`,
+                // Menentukan gambar latar sesuai dengan mode yang digunakan (dark mode atau light mode)
+                backgroundImage: `url(${
+                    isDarkMode ? backgroundDark.src : backgroundLight.src
+                })`,
             }}
         >
-            {children}
+            {children} {/* Menampilkan children di dalam div */}
         </div>
     );
 }
